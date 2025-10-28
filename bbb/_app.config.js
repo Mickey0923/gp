7d32de3b6cfc93918e9318c39b1d5a3e26348443
Mickey0923 <474792066@qq.com>
2025-10-28 15:32:09 +0800

    first commit

---
+++ bbb/_app.config.js
@@ -0,0 +1,1 @@
+window.__PRODUCTION__SMART_COMMUNITY__CONF__={"VITE_GLOB_APP_TITLE":"市中区共享单车监管数据大屏","VITE_GLOB_APP_SHORT_NAME":"smart_community","VITE_GLOB_API_URL":"https://apigw.ankechain.cn/bkbig","VITE_GLOB_API_URL_PREFIX":""};Object.freeze(window.__PRODUCTION__SMART_COMMUNITY__CONF__);Object.defineProperty(window,"__PRODUCTION__SMART_COMMUNITY__CONF__",{configurable:false,writable:false,});
